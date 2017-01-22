ec60da9eb2a64be9a8ff1e7cde736c5e1c8fdf63
Apaar Madan <apaar_madan@hotmail.com>
2017-01-22 17:16:12 -0500

    sounds, etc

--- src/audio.js
+++ src/audio.js
@@ -2,6 +2,11 @@ const BEATS_PER_MINUTE = 120;
 const TIME_PER_BEAT = 60 / BEATS_PER_MINUTE;
 
 var ost = document.getElementById("ost");
+ost.addEventListener("ended", function() {
+    this.currentTime = 0;
+    this.play();
+}, false);
+
 var redSound = document.getElementById("red");
 var blueSound = document.getElementById("blue");
 var yellowSound = document.getElementById("yellow");
